18a370f7d362ba9263c858908458cc7dad12b272
Yun4g <delightvincent487@gmail.com>
2024-09-03 11:28:54 +0100

    weather app

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
- const apiKey = "982386a8060d0b42a7e4726e7c595ab5";
- const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
+const apiKey = "982386a8060d0b42a7e4726e7c595ab5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const weatherData = async (city) => {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
 
- const weatherData = async (city) =>{
-       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-       const data = await response.json();
-      console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
 
-
-      document.querySelector(".city").innerHTML = data.name;
-      document.querySelector(".temp").innerHTML = Math.round( data.main.temp) +"°c";
-      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-      document.querySelector(".wind").innerHTML = data.wind.speed+ " km/h ";
-    }
-
-const search = () =>{
-   let searchValue = document.querySelector("#search");
+const search = () => {
+    let searchValue = document.querySelector("#search");
     weatherData(searchValue.value);
-} 
-
+}
 
 
 
