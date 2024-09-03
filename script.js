501b1cc3373737761424654c5f612db7684e256f
Yun4g <delightvincent487@gmail.com>
2024-09-03 10:34:30 +0100

    Weather app

--- script.js
+++ script.js
@@ -2,21 +2,25 @@
  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
 
 
- let  searchInput = document.querySelector(".search input");
-const checkWeather = async (city) => {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
-
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) +"°c" ;
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-}
+ const weatherData = async (city) =>{
+       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+       const data = await response.json();
+      console.log(data);
 
 
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML = Math.round( data.main.temp) +"°c";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed+ " km/h ";
+    }
+
+const search = () =>{
+   let searchValue = document.querySelector("#search");
+    weatherData(searchValue.value);
+} 
 
 
-const search = ()=>{
-    checkWeather(searchInput.value);
-}
+
+
+
+
